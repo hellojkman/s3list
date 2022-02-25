d6fe9b9ae6fdf018ca1787d16f8d4f82c915d0bf
hellojkman <sun-up12@hanmail.net>
2022-02-25 12:30:11 +0000

    s3_list

---
+++ routes/restful.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+const users = [
+  { id:1, name:"user1"},
+  { id:2, name:"user2"},
+  { id:3, name:"user3"}
+];
+
+// simple api
+app.get("/Hello", (req, res) => {
+  res.send("주영이 바보");
+})
+
+// request param x, response o
+app.get("/api/users", (req, res) => {
+  res.json({ok:true, users:users});
+})
+
+// Query parameter, request parm o, response o
+app.get("/api/users/user", (req, res) => {
+  const user_id = req.query.user_id
+  const user = users.filter(data => data.id == user_id);
+  res.json({ok:false, users:user});
+ })
+
+// path parameter, request parm o, response o
+app.get("/api/users/:user_id", (req, res) => {
+  const user_id = req.params.user_id
+  const user = users.filter(data => data.id == user_id);
+  res.json({ok:false, users:user});
+})
+
+// post, request body, response o
+app.post("/api/users/userBody", (req, res) => {
+  const user_id = req.body.id
+  const user = users.filter(data => data.id == user_id);
+  res.json({ok:true, users:user});
+})
+
+// post, request body, response o
+app.post("/api/users/add", (req, res) => {
+  const { id, name } = req.body
+  const user = users.concat({id, name});
+  res.json({ok:true, users:user});
+})
+
+// put, request body, response o
+app.put("/api/users/update", (req, res) => {
+  const { id, name } = req.body
+  const user = users.map(data => {
+    if(data.id == id) data.name = name
+    return {
+      id : data.id,
+      name : data.name
+    }
+  })
+  res.json({ok:true, users:user});
+})
+
+// patch, request path param & body, response o
+app.patch("/api/user/update/:user_id", (req, res) => {
+  const { user_id } = req.params
+  const { name } = req.body
+  const user = users.map(data => {
+    if(data.id == user_id) data.name = name
+    return {
+      id : data.id,
+      name : data.name
+    }
+  })
+  res.json({ok:true, users:user});
+})
+
+// delete, request body, response o
+app.delete("/api/user/delete", (req, res) => {
+  const { user_id } = req.body
+  const user = users.filter(data => data.id != user_id);
+  res.json({ok:true, users:user});
+})
+
+module.exports = app;
